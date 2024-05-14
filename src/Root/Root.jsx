f8b82d6c6974579b92454fdee1f9be987fb6097d
rakibhow203 <rakibhossainhow65@gmail.com>
2024-05-15 01:33:59 +0600

    added galery img

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "../pages/Home/NavBar/NavBar";
 import Footer from "../pages/Home/Footer/Footer";
+import { useEffect } from "react";
 
 
 const Root = () => {
+  const loc = useLocation();
+  useEffect(() => {
+    if (loc.pathname === '/') {
+      document.title = `TC-Boil - HOME`;
+    } else {
+      document.title = `TC-Boil ${loc.pathname.replace('/', '-')}`;
+    }
+  }, [loc.pathname]);
   return (
     <div>
 
--- src/pages/Gallery/Photo.jsx
+++ src/pages/Gallery/Photo.jsx
@@ -1,17 +1,18 @@
 
+
 const Photo = ({ card }) => {
-  const { image } = card
+  const { image, name, category } = card;
+
   return (
-    <div>
-      <div className="mb-4">
-        <div className="relative max-w-xs overflow-hidden bg-cover bg-no-repeat">
-          <img
-            src={image}
-            className="max-w-xs w-full h-52 rounded-lg"
-            alt="Louvre"
-          />
-          <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-indigo-700 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-50"></div>
-        </div>
+    <div className="relative group w-full h-52">
+      <img
+        src={image}
+        alt="Sample"
+        className="w-full   h-52 rounded-lg object-cover"
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity duration-300">
+        <div><span className="text-white text-xl">{name}</span></div>
+
       </div>
     </div>
   );
