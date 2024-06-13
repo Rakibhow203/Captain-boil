5670be3f4129c0f9524d437b9bffedab9150c7ec
rakibhow203 <rakibhossainhow65@gmail.com>
2024-06-13 22:34:26 +0600

    updated top food section

--- src/pages/Home/TopFoods/ShowTopFoods.jsx
+++ src/pages/Home/TopFoods/ShowTopFoods.jsx
@@ -1,28 +1,31 @@
+
 import { Link } from "react-router-dom";
 
-
 const ShowTopFoods = ({ card }) => {
-  const {
-    category, image, name, price, _id } = card
+  const { category, image, name, price, _id } = card;
+
   return (
-    <div>
-      <div>
-        <div className="card w-96 bg-white shadow-2xl  mb-2 mt-2">
-          <figure className="px-10 pt-10 h-44 ">
-            <img src={image} alt="Shoes" className="rounded-xl" />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title text-black font-extrabold"><span className="font-bold">Name:</span> {name} </h2>
-            <p className="text-black font-bold"> Category: {category} </p>
-            <p className="text-black font-bold">Price: {price} </p>
-
-            <div className="flex gap-10">
-              <div className="card-actions">
-                <Link to={`/Top/${_id}`}><button className="btn btn-primary bg-gradient-to-r from-green-400 to-orange-500 hover:from-pink-500 hover:to-yellow-500">Get Details</button></Link>
-              </div>
-
-            </div>
-
+    <div className="flex justify-center items-center p-4">
+      <div className="card w-96 bg-white shadow-2xl rounded-xl overflow-hidden transform transition duration-500 hover:scale-105 mb-4 mt-4 hover:shadow-lg hover:shadow-yellow-500/50">
+        <figure className="px-10 pt-10 h-44 flex justify-center items-center overflow-hidden">
+          <img
+            src={image}
+            alt={name}
+            className="rounded-xl h-full object-cover transition-transform duration-500 ease-in-out transform hover:scale-110"
+          />
+        </figure>
+        <div className="card-body items-center text-center p-4">
+          <h2 className="card-title text-gray-800 font-extrabold text-xl mb-2">
+            <span className="font-bold">Name:</span> {name}
+          </h2>
+          <p className="text-gray-700 font-bold mb-2">Category: {category}</p>
+          <p className="text-gray-700 font-bold mb-4">Price: ${price}</p>
+          <div className="card-actions">
+            <Link to={`/Top/${_id}`}>
+              <button className="btn btn-primary bg-gradient-to-r from-green-400 to-orange-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-4 rounded-full transition duration-300">
+                Get Details
+              </button>
+            </Link>
           </div>
         </div>
       </div>
